9edaea967087abbab6afa68d7f7170b9e0217679
Samuel Gerber <sgerber@math.duke.edu>
2015-06-10 12:26:01 -0400

    missed update

--- Rscripts/multiscale.entropy.R
+++ Rscripts/multiscale.entropy.R
@@ -2,37 +2,43 @@
 extract.segs <- function(X, time, k){
   
   n = nrows(X)
-  S = c()
+  S1 = c()
+  S2 = c()
   for(i in 1:ncol(X)){
     for(j in 1:k){
-      s = seq(j, n-k+j, by=offset)
-      S = cbind( S, X$[s, i])
+      s = seq(j, n-k+j, by=k)
+      S1 = cbind( S1, X$[s, i])
+      if(j != k){
+        S2 = cbind( S2, X$[s, i])
+      }
     }
   } 
   if(rm.jumps){
     tj = time[k:n] - time[1:(n-k+1)] 
-    s = seq(j, n-k+j, by=offset)
-  
-    ind  = which(jump == ( k-1))
-    S = S[ind, ]
+    ind = which(jump == ( k-1))
+    S1 = S1[ind, ]
+    S2 = S2[ind, ]
   }
 
-  S
+  list(S1=S1, S2=S2)
 }
 
-multiscale.entropy <- function(X, time, max.Smooth=16){
+multiscale.entropy <- function(X, k=3,time, max.Smooth=16){
 
   source("extract.features.R")
 
-  threshold
-
-
+  threshold = 0;
   sigma=1
   Xs = X
   for(i in 1:max.Smooth){
-    extract.segs(X
-    D <- dist(X, method="Euclidean")
-
+    S = extract.segs(X, time, k)
+    D1 <- dist(S$S1, method="Maximum")
+    D2 <- dist(S$S2, method="Maximum")
+    if(threshold = 0){
+      threshold = sqrt( var(D1) ) * 0.15
+    }
+    
+    C1 = rowSums(as.matrix(D1) < threshold
 
 }
 
