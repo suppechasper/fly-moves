899b4edac7bf70fe2da435dbc76d9ef087261311
Samuel Gerber <sgerber@math.duke.edu>
2015-06-23 15:37:32 -0400

    entropy update

--- Rscripts/analysis.entropy.R
+++ Rscripts/analysis.entropy.R
@@ -17,7 +17,9 @@ scales = 20
 
 #extract features
 
-Elist <- list()
+E1list <- list()
+E2list <- list()
+E3list <- list()
 for(k in 1:length(flies)){
  source(flies[[k]])
 
@@ -27,38 +29,51 @@ for(k in 1:length(flies)){
 
   Z <- list()
   for( i in 1:length(Slist) ){
-    Z[[i]] = matrix( Slist[[i]]$lengths, ncol=1) 
-#Z[[i]] = matrix(abs(Slist[[i]]$curvatureMean), ncol=1) 
+   #Z[[i]] = matrix( Slist[[i]]$lengths, ncol=1) 
+   #Z[[i]] = matrix(abs(Slist[[i]]$curvatureMean), ncol=1) 
+   Z[[i]] = scale( cbind( Slist[[i]]$lengths, Slist[[i]]$curvatureMean ) )
   }
 
   O <- list()
   for(i in 1:length(Z) ){
-    O[[i]] <- extract.condition.odor2( Z[[i]], Slist[[i]]$C ) 
+    O[[i]] <- as.matrix( extract.condition.odor2( Z[[i]], Slist[[i]]$C ) )
   }
 
 
-  E <- list()
+  E1 <- list()
+  E2 <- list()
+  E3 <- list()
   for(i in 1:length(O[[1]]) ){
-    E[[i]] <- matrix(0, ncol=scales, nrow=length(O) )
+    E1[[i]] <- matrix(0, ncol=scales, nrow=length(O) )
+    E2[[i]] <- matrix(0, ncol=scales, nrow=length(O) )
+    E3[[i]] <- matrix(0, ncol=scales, nrow=length(O) )
     for(j in 1:length(O) ){
       if(length(O[[j]][[i]]) < 1000){
-        E[[i]][j, ] = NA
+        E1[[i]][j, ] = NA
+        E2[[i]][j, ] = NA
+        E3[[i]][j, ] = NA
       }
       else{
-        E[[i]][j, ] <- multiscale.entropy(matrix(O[[j]][[i]], ncol=1), 2, scales,
-           0.2)[1, ]
+        tmp <- multiscale.entropy( O[[j]][[i]], 2, scales, 0.25)
+        E1[[i]][j, ] <- tmp[1, ]
+        E2[[i]][j, ] <- tmp[2, ]
+        E3[[i]][j, ] <- tmp[3, ]
       } 
     }
   }
 
-  Elist[[k]] = E
+  E1list[[k]] = E1
+  E2list[[k]] = E2
+  E3list[[k]] = E3
 }
 
 
-plot(NA, xlim=c(1, scales), ylim= c(1.2, 2.1), bty="n", xlab="scale", ylab="sample entropy" )  
+plot(NA, xlim=c(1, scales), ylim= c(0.3, 1.8), bty="n", xlab="scale", ylab="sample entropy" )  
 cols = brewer.pal(n=3, "Dark2")
 legend=c()
 lty=c()
+
+Elist <- E3list
 for(k in 1:length(Elist) ){
   E = Elist[[k]]
 
